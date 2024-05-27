dd747112b2281e9de41200713b0b67f8e87f9dca
pmh2029 <hieu.pm2029@gmail.com>
2024-05-27 09:19:32 +0700

    fix timezone

--- internal/pkg/domains/models/entities/comments.go
+++ internal/pkg/domains/models/entities/comments.go
@@ -1,12 +1,34 @@
 package entities
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Comment struct {
-	ID      int     `json:"id" binding:"required"`
-	Rate    int `json:"rate"`
-	Comment string  `json:"comment"`
-	UserID  int     `json:"user_id"`
-	User    User    `gorm:"foreignKey:UserID;references:ID" json:"user"`
-	PlaceID int     `json:"place_id"`
-	Place   Place   `gorm:"foreignKey:PlaceID;references:ID" json:"place"`
+	ID      int    `json:"id" binding:"required"`
+	Rate    int    `json:"rate"`
+	Comment string `json:"comment"`
+	UserID  int    `json:"user_id"`
+	User    User   `gorm:"foreignKey:UserID;references:ID" json:"user"`
+	PlaceID int    `json:"place_id"`
+	Place   Place  `gorm:"foreignKey:PlaceID;references:ID" json:"place"`
 	BaseEntity
+}
+
+func (i *Comment) AfterFind(tx *gorm.DB) (err error) {
+	local, err := time.LoadLocation("Asia/Ho_Chi_Minh")
+	if err != nil {
+		return
+	}
+
+	createdAtTimeLocal, err := time.ParseInLocation("2006-01-02 15:04:05", i.CreatedAt.Format("2006-01-02 15:04:05"), local)
+	if err != nil {
+		return
+	}
+
+	i.CreatedAt = &createdAtTimeLocal
+
+	return
 }
