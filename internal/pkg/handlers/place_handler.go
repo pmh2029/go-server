9bb7b2bc48c3da073011f4ace57a9ba1d254205a
pmh2029 <hieu.pm2029@gmail.com>
2024-06-12 13:34:11 +0700

    update

--- internal/pkg/handlers/place_handler.go
+++ internal/pkg/handlers/place_handler.go
@@ -573,7 +573,7 @@ func (h *placeHandler) ListSuggestPlace(c *gin.Context) {
 			SELECT *,
 			(6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance
 			FROM places
-			WHERE name COLLATE utf8_general_ci LIKE ?
+			WHERE LOWER(name) LIKE LOWER(?)
 			ORDER BY distance ASC, rate DESC;
 			`
 			err := h.db.Raw(query, latitude, longitude, latitude, "%"+keyword+"%").Scan(&places).Error
