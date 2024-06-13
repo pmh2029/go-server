f34852ef8270a0aa3f41683a8164bdbb3130c056
pmh2029 <hieu.pm2029@gmail.com>
2024-06-13 10:51:28 +0700

    fix

--- internal/pkg/domains/models/dtos/trip_dto.go
+++ internal/pkg/domains/models/dtos/trip_dto.go
@@ -6,11 +6,11 @@ type CreateTripRequestDto struct {
 	Owner    int                   `json:"owner" binding:"required,min=1"`
 	Name     string                `json:"name" binding:"required,min=1"`
 	FromDate int                   `json:"from_date" binding:"required,min=1"`
-	ToDate   int                   `json:"to_date" binding:"required,min=1,gtfield=FromDate"`
+	ToDate   int                   `json:"to_date" binding:"required,min=1"`
 	Users    int                   `json:"users"`
 	Days     []CreateDayRequestDto `json:"days" binding:"required"`
 }
 
 type CreateDayRequestDto struct {
-	Places []entities.PlaceInDay `json:"places" binding:"required"`
+	Places []entities.PlaceInDay `json:"places"`
 }
